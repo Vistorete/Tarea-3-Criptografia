0cbb4b8fcffd13cb21fd7aa4aeeb791f0a85a9e3
Weebtor <vistorete502@gmail.com>
2020-10-13 01:10:38 -0300

    hex

--- script.py
+++ script.py
@@ -3,18 +3,18 @@ if __name__ == "__main__":
     
     from Crypto.Cipher import DES
     from Crypto import Random
-    import base64
+
     key = b'-8B key-'
     iv = Random.new().read(DES.block_size)
     cipher = DES.new(key, DES.MODE_CBC, iv)
     plaintext = b'sona si latine loqueris '
     msg = iv + cipher.encrypt(plaintext)
     
-    iv64 = base64.b64encode(iv)
-    msg64 = base64.b64encode(msg)
-    print(plaintext.decode())
-    print(iv64.decode())
-    print(msg64.decode())
+    ivhex = iv.hex()
+    msghex = msg.hex()
+    print(plaintext)
+    print(ivhex)
+    print(msghex)
     index = open("index.html","w")
     index.write('''
         <html>
@@ -26,11 +26,11 @@ if __name__ == "__main__":
             </title>
             <body>
                 <p>Este sitio contiene un mensaje secreto</p>
-                <div class="des64" id="%s"></div>
-                <div class="iv64" id="%s"></div>
+                <div class="deshex" id="%s"></div>
+                <div class="ivhex" id="%s"></div>
                 <div class="key" id="%s"></div>
             </body>
         </html>
-        ''' % (msg64.decode(), iv64.decode(), key.decode()))
+        ''' % (msghex, ivhex, key))
     index.close()
 
